b34534f5637066e174b77b6e352de59e18deccc1
Santho-D <david.stoddart@protonmail.com>
2026-01-07 17:59:13 +0100

    Excercise

--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
-﻿using System;
-
-Random CoinFlip = new Random();
-int Coin = CoinFlip.Next(0, 2);
+﻿string permission = "Admin|Manager";
+int level = 55;
 
 
-Console.WriteLine ($"You've got: " + (Coin == 0 ? "Heads" : "Tails"));
+if (permission.Contains("Admin")) 
+{
+    if (level >= 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}    
+    else if (permission.Contains("Manager"))
+    {
+        if (level >= 20)
+        {
+            Console.WriteLine("Conatct Admin for access.");
+        }
+        else
+        {    
+            Console.WriteLine("You do not have sufficient privileges.");
+        }
+    }
+else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+    
--- obj/Debug/net10.0/Testproj.AssemblyInfo.cs
+++ obj/Debug/net10.0/Testproj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testproj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8077947d3ce038a970ec6fb211e661206b380419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afbfc58bae561417dc8d0835b2c7d4f7ae61064d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testproj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testproj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
